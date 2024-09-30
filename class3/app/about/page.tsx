04277729169e9a00f7f4ac057bb7723c13006d9d
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-09-30 23:44:27 +0500

    Add files via upload

---
+++ class3/app/about/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/compnent/Navbar"
+
+export default function About(){
+    return( 
+        <>
+        <h1>About US Page</h1>
+        {/* <Navbar/> */}
+        </>
+    );
+}
---
+++ class3/app/page.tsx
@@ -0,0 +1,11 @@
+import react from "react"
+
+export default function Home(){
+    const name:string = "Beena Anum"
+    return(
+        <><h1>MY name is {name}</h1>
+        <p>Hello Everyone, This is my first Next.js Project.<br></br> 
+        I created simple Navbar , linking and routing</p>
+         </>
+    )
+}
