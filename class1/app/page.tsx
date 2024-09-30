a09d4bf69f1cd8bab293370add75164928f141c4
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-09-30 22:45:21 +0500

    Add files via upload

---
+++ class1/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+function Homepage() {
+  return (                         
+      <> 
+   <strong><h1 className="flex  justify-center bg-red-500 ">Hello World!</h1> <br />
+       <h2 className="flex justify-center bg-green-500"> I'm Developer.</h2>
+       <h2 className="flex justify-center bg-yellow-600">This is my 1st next.js project.</h2>
+   </strong>
+       
+      </>
+  );
+}
+export default Homepage
