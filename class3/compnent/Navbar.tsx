fdee4b3fe4d74497e9e3680514364d5d437c4446
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-03 15:49:35 +0500

    Update Navbar.tsx

--- class3/compnent/Navbar.tsx
+++ class3/compnent/Navbar.tsx
@@ -1,30 +1,4 @@
 import Link from "next/link";
-//2nd class
-// export default function Navbar(){
-//     return(
-//        <ul className="navbar flex justify-center items-center gbp-4">
-//             <li><Link href="/"  className="nav-link bg-black-400"> 
-//             Home
-//            </Link></li>
-//             <li><Link href="/about" > 
-//             About
-//            </Link></li>
-          
-//             <li><Link href="/contact" >
-//             Contact
-//            </Link></li>
-          
-//             <li><Link href="/feedback"> 
-//             Feedback
-//            </Link></li>
-          
-//             <li><Link href="/country"> 
-//             Country
-//            </Link></li>
-//           </ul>
-//     )
-
-// }
 //3rd class
 export default function Page(){
     return(
