4aadee9c45a7af07a38b5753d250bdb9ea62015f
AI Anam <160389592+BeenaAnum@users.noreply.github.com>
2024-10-04 13:47:24 +0500

    Update layout.tsx

--- class3/app/layout.tsx
+++ class3/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 // import { Inter } from "next/font/google";
 //import "./globals.css";
 // import { link } from "fs";
-import Navbar from "@/compnent/Navbar";
+import Navbar from "./compnent/Navbar";
 
 // const inter = Inter({ subsets: ["latin"] });
 
